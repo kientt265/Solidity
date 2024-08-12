32ac09a66d0f957b22c3291797e288a1fbddb138
kientt265 <kientran26503@gmail.com>
2024-08-12 17:46:07 +0700

    ether to contract

---
+++ contracts/Projects/SampleContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+contract SampleContract{
+    string public myString= "Hello World!";
+    function updateString(string memory _newString) public payable  {
+        if(msg.value == 1 ether) {
+            myString =_newString;
+        } else {
+            payable(msg.sender).transfer(msg.value);
+        }
+    }
+    }
